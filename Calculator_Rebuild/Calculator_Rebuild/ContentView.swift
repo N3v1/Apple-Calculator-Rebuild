7516a2b29116347165ce656843b17631e8759f08
N3v1 <n3v1010@gmail.com>
2023-07-30 18:50:25 +0200

    Setting up ContentView

--- Calculator_Rebuild/Calculator_Rebuild/ContentView.swift
+++ Calculator_Rebuild/Calculator_Rebuild/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                //MARK: RESULT AREA
+                Text("Hello, world!")
+                
+                //MARK: BUTTONS
+                
+            }
         }
-        .padding()
     }
 }
 
