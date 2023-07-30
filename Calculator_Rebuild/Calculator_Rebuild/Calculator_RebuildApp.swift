7e04032de0b7b98adad67b2d038bef3df0ddc4e1
N3v1 <n3v1010@gmail.com>
2023-07-30 18:41:27 +0200

    Set-Up project

---
+++ Calculator_Rebuild/Calculator_Rebuild/Calculator_RebuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_RebuildApp.swift
+//  Calculator_Rebuild
+//
+//  Created by Nevio Hirani on 30.07.23.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_RebuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
