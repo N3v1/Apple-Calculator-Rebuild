dd969df7bf8dfd24de6a99ef565f8b4f2369ab8c
N3v1 <n3v1010@gmail.com>
2023-07-31 09:07:38 +0200

    Fix typo and add background Color

--- Calculator_Rebuild/Calculator_Rebuild/ContentView.swift
+++ Calculator_Rebuild/Calculator_Rebuild/ContentView.swift
@@ -11,6 +11,17 @@ enum CalcButton: String {
     case one, two, three, four, five, six, seven, eight, nine, zero
     case equal, plus, minus, multiply, division
     case clear, plusMinus, percent, decimal
+    
+    var background: Color {
+        switch self {
+        case .one, .two, .three, .four, .five, .six, .seven, .eight,. nine, .zero, .decimal:
+            return Color(UIColor(red: 55/255.0, green: 55/255.0, blue: 55/255.0, alpha: 1))
+        case .equal, .plus, .minus, .multiply, .division:
+            return Color(.orange)
+        case .clear ,.plusMinus, .percent:
+            return Color(.lightGray)
+        }
+    }
 }
 
 struct ContentView: View {
@@ -18,7 +29,7 @@ struct ContentView: View {
     let buttons: [[CalcButton]] = [
         [.clear, .plusMinus, .percent, .division],
         [.seven, .eight, .nine, .multiply],
-        [.four, .five, .six, .seven],
+        [.four, .five, .six, .minus],
         [.one, .two, .three, .plus],
         [.zero, .decimal, .equal]
     ]
@@ -42,12 +53,12 @@ struct ContentView: View {
                 // MARK: BUTTONS
                 ForEach(buttons, id: \.self) { row in
                     HStack(spacing: 12) {
-                        ForEach(row, id: \.self) { items in
-                            Text(items.rawValue)
+                        ForEach(row, id: \.self) { button in
+                            Text(button.rawValue)
                                 .foregroundColor(.white)
                                 .font(.system(size: 32))
                                 .frame(width: self.buttonWidth(), height: self.buttonWidth())
-                                .background(Color.orange)
+                                .background(button.background)
                                 .cornerRadius(self.buttonWidth())
                         }
                     }
